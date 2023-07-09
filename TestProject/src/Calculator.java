e6c92289a7d20e4bf0e229dd44a1ba9b3a06bb23
theri <theri@LAPTOP-S6E0L340>
2023-07-09 19:21:58 +0530

    second commit

---
+++ TestProject/src/Calculator.java
@@ -0,0 +1,16 @@
+
+public class Calculator {
+
+	int a=5;
+	int b=2;
+	
+	public void add() {
+		System.out.println("sum is:"+(a+b));
+	}
+	public static void main(String[] args) {
+		
+		Calculator cal=new Calculator();
+		cal.add();
+	}
+
+}
